089f82bf2d4268be457ebc86d9aa3583b57504d9
Shin-Yan <sdfghlklk@gmail.com>
2022-07-14 16:57:48 +0800

    final version of backpropagation

--- Lab1/main.py
+++ Lab1/main.py
@@ -59,7 +59,7 @@ def tanh(x):
     return np.tanh(x)
 
 def derivative_tanh(x):
-    return 1.0 - y**2
+    return 1.0 - x**2
 
 def relu(x):
     return np.maximum(0.0, x)
@@ -80,6 +80,7 @@ def derivative_leaky_relu(x):
 
 #loss functions
 def mse_loss(prediction, ground_truth):
+    # print(f'pred ={prediction.transpose()}')
     return np.mean((prediction - ground_truth) ** 2)
 
 def mse_loss_derivative(prediction, ground_truth):
@@ -92,12 +93,20 @@ class Layer:
         self.activation = activation
         self.optimizer = optimizer
         self.learning_rate = learning_rate
+        self.momentum = 0
+        self.n = 0
 
     def forward_pass(self, inputs):
         forward_value = np.append(inputs, np.ones((inputs.shape[0],1)),axis = 1)
         self.forward_output = None
         if self.activation == 'sigmoid':
             self.forward_output = sigmoid(np.matmul(forward_value, self.weight))
+        elif self.activation == 'tanh':
+            self.forward_output = tanh(np.matmul(forward_value, self.weight))
+        elif self.activation == 'relu':
+            self.forward_output = relu(np.matmul(forward_value, self.weight))
+        elif self.activation == 'leaky_relu':
+            self.forward_output = leaky_relu(np.matmul(forward_value, self.weight))
         else:
             self.forward_output = np.matmul(forward_value,self.weight)
 
@@ -108,19 +117,32 @@ class Layer:
         self.backward_output = None
         if self.activation == 'sigmoid':
             self.backward_output = np.multiply(derivative_sigmoid(self.forward_output),inputs)
+        elif self.activation == 'tanh':
+            self.backward_output = np.multiply(derivative_tanh(self.forward_output), inputs)
+        elif self.activation == 'relu':
+            self.backward_output = np.multiply(derivative_relu(self.forward_output), inputs)
+        elif self.activation == 'leaky_relu':
+            self.backward_output = np.multiply(derivative_leaky_relu(self.forward_output), inputs)
         else:
             self.backward_output = inputs
-        return np.matmul(self.backward_output, self.weight[:-1].T)
+        return np.matmul(self.backward_output, self.weight[:-1].transpose())
 
     def learn(self):
-        gradient = np.matmul(self.forward_value.T, self.backward_output)
+        gradient = np.matmul(self.forward_value.transpose(), self.backward_output)
+        beta = 0.9
+        epsilon = 10 ** (-8)
         if self.optimizer == 'gd':
             weight_change = -self.learning_rate * gradient
-            # print(weight_change)
+        elif self.optimizer == 'momentum':
+            self.momentum = beta * self.momentum - self.learning_rate * gradient
+            weight_change = self.momentum
+        elif self.optimizer == 'adagrad':
+            self.n += np.square(gradient)
+            weight_change = -self.learning_rate * np.multiply(1/(np.sqrt(self.n + epsilon)), gradient)
         self.weight += weight_change
 
 class NeuralNetwork:
-    def __init__(self,epoch, learning_rate, layers, inputs, hidden_units, activation , optimizer):
+    def __init__(self, epoch, learning_rate, layers, inputs, hidden_units, activation, optimizer):
         self.epoch = epoch
         self.learning_rate = learning_rate
         self.hidden_units = hidden_units
@@ -133,8 +155,7 @@ class NeuralNetwork:
 
         for i in range(layers -1):
             self.layers.append(Layer(hidden_units, hidden_units, activation, optimizer, learning_rate))
-
-        self.layers.append(Layer(hidden_units, 1, activation, optimizer, learning_rate))
+        self.layers.append(Layer(hidden_units, 1, 'sigmoid', optimizer, learning_rate))
 
     def forward(self, inputs):
         for layer in self.layers:
@@ -156,7 +177,7 @@ class NeuralNetwork:
             self.backward(mse_loss_derivative(prediction, ground_truth))
             self.learn()
 
-            if epoch %10 == 0:
+            if epoch %100 == 0:
                 # print('inputs=',inputs,'ground_truth=',ground_truth)
                 # print('prediction = ',prediction)
                 
@@ -177,16 +198,16 @@ class NeuralNetwork:
 def main():
     inputs1, label1 = generate_linear()
     inputs2, label2 = generate_XOR_easy()
-    network = NeuralNetwork(epoch = 1000, learning_rate = 0.05, layers = 2, inputs = 2, hidden_units = 4,
-                            activation = 'No', optimizer = 'gd')   
-    prediction = network.train(inputs1, label1)
-    show_result(inputs1, label1, prediction,'linear.png')
-    network.show_learning_curve('linear')
-    print('Accuracy : ', float(np.sum(prediction == label1)) / len(label1))
-    # prediction = network.train(inputs2, label2)
-    # show_result(inputs2, label2,prediction,'XOR.png')
-    # network.show_learning_curve('XOR')
-    # print('Accuracy : ', float(np.sum(prediction == label2)) / len(label2))
+    network = NeuralNetwork(epoch = 1000000, learning_rate = 0.05, layers = 2, inputs = 2, hidden_units = 4,
+                            activation ='leaky_relu',optimizer = 'gd')   
+    # prediction = network.train(inputs1, label1)
+    # show_result(inputs1, label1, prediction,'linear.png')
+    # network.show_learning_curve('linear')
+    # print('Accuracy : ', float(np.sum(prediction == label1)) / len(label1))
+    prediction = network.train(inputs2, label2)
+    show_result(inputs2, label2,prediction,'XOR.png')
+    network.show_learning_curve('XOR')
+    print('Accuracy : ', float(np.sum(prediction == label2)) / len(label2))
 
 if __name__ == '__main__':
     main()
